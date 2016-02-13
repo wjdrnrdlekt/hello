12721b64ecf26e9f23c15ed97a608c9d6a14e223
wjdrnrdlekt <tjwjdrnr666@naver.com>
2016-02-13 18:56:16 +0900

    test

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,12 +4,12 @@ int main(void)
 {
 	int num=20;
 	std::cout<<"Hello World!"<<std::endl;
-	std::cout<<"Git Test" << endl;
-	std::cout<<"Git Test" << endl;
-	std::cout<<"Git Test" << endl;	
+	std::cout<<"Git Test2" << endl;
+	std::cout<<"Git Test2" << endl;
+	std::cout<<"Git Test2" << endl;	
 
 	
-	std::cout<<"Git Test" << endl;
+	std::cout<<"Git Test2" << endl;
 	std::cout<<"Git Test" << endl;	
 	return 0;
 }
