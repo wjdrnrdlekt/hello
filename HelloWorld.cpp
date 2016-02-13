bcebe975fb82ad3db402e04ac443aa9af99e95a5
wjdrnrdlekt <tjwjdrnr666@naver.com>
2016-02-13 18:37:32 +0900

    1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,6 @@ int main(void)
 	int num=20;
 	std::cout<<"Hello World!"<<std::endl;
 	std::cout<<"Hello "<<"World!"<<std::endl;
-	std::cout<<num<<' '<<'A';
-	std::cout<<' '<<3.14<<std::endl;
+	std::cout<<"Git Test" << endl;
 	return 0;
 }
